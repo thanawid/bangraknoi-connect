1fcff941dff8e517173b8c6dac3404a7fb350984
Bangraknoi <78256158+thanawid@users.noreply.github.com>
2026-06-28 20:45:19 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,29 @@
-const CACHE = 'brn-connect-v5';
+const CACHE = 'brn-connect-v6';
 const CORE = [
   './', './index.html', './styles.css', './app.js', './data.js', './manifest.webmanifest',
-  './assets/master-scene.png', './assets/mascot_hero.png', './assets/bc-mascot.png', './assets/bc-logo.png', './assets/favicon.png',
-  './assets/categories/01.png', './assets/categories/02.png', './assets/categories/03.png', './assets/categories/04.png',
-  './assets/categories/05.png', './assets/categories/06.png', './assets/categories/07.png', './assets/categories/08.png'
+  './assets/master-scene.png', './assets/mascot_hero.png', './assets/bc-logo.png', './assets/favicon.png',
+  './assets/mascot_chang.png', './assets/mascot_food.png', './assets/mascot_travel.png',
+  './assets/mascot_career.png', './assets/mascot_news.png', './assets/mascot_event.png',
+  './assets/mascot_product.png', './assets/mascot_chat.png', './assets/mascot_service.png',
+  './assets/categories/01.png', './assets/categories/02.png', './assets/categories/03.png',
+  './assets/categories/04.png', './assets/categories/05.png', './assets/categories/06.png',
+  './assets/categories/07.png', './assets/categories/08.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(CORE))
+      .then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
