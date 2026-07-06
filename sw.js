693ae080b90dcdb85489852c9dce86769cd66eeb
Bangraknoi <78256158+thanawid@users.noreply.github.com>
2026-07-06 20:55:35 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bangraknoi-connect-v25-simplified-service-menu';
+const CACHE_NAME = 'bangraknoi-connect-v26-clean-hero-video';
 const PRECACHE_URLS = [
   './',
   './index.html',
